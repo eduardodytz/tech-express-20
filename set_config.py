7c96b514d8d6bef47eee805b2f7c5eb282d0e774
Eduardo Dytz <eduardo.dytz@gmail.com>
2020-11-05 11:10:20 -0300

    first commit

---
+++ set_config.py
@@ -0,0 +1,31 @@
+# Importing a library
+from netmiko import ConnectHandler
+
+# Create a dictionary representing the device
+device = {
+    'device_type': 'cisco_xe',
+    'host':   '10.10.20.48',
+    'username': 'developer',
+    'password': 'C1sco12345',
+    'port' : 22
+}
+
+# Execute configuration change commands (will automatically enter into config mode)
+config_commands = [
+    'interface loopback10',
+    'no shut'
+]
+
+# Establish an SSH connection to the device by passing in the device dictionary
+net_connect = ConnectHandler(**device)
+
+
+# send_command for show commands
+output = net_connect.send_command('show ip int brief')
+
+
+# send_config_set for commands in config Mode
+#output = net_connect.send_config_set(config_commands)
+
+# Print output
+print(output)
